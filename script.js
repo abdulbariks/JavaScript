383748050b44b84502b803c88c80eed419ee0ce2
Abdul Barik <abdulbarik1997m@gmail.com>
2023-10-01 00:24:40 +0600

    Javascript Variables

---
+++ script.js
@@ -0,0 +1,17 @@
+var a =10;
+var b = 20;
+var c = a+b;
+console.log(c);
+
+let number1 =10;
+let number2 = 20;
+let sum = number1+number2;
+console.log(sum);
+
+const number3 =10;
+const number4 = 20;
+const sub = number3-number4;
+console.log(sub);
+
+
+
