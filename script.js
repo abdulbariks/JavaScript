3dc7a6f11f9e47aaf7455c86ffade0176ca54df2
Abdul Barik <abdulbarik1997m@gmail.com>
2023-10-03 00:38:28 +0600

    Template Literals

--- script.js
+++ script.js
@@ -1,74 +1,13 @@
-// JavaScript Data Types
- // Numbers:
-let lengt = 16;
-let weight = 7.5;
+// Template Literals
+// Backtick ``
 
-// Strings:
-let color = "Yellow";
-let lastName = "Barik";
+let name = "Barik"
+let age = 26;
+let dob = "03 Apil 1997"
 
-// Booleans
-let x = true;
-let y = false;
-
-// Object:
-const person = {firstName:"Abdul", lastName:"Barik"};
-
-// Array object:
-const cars = ["Saab", "Volvo", "BMW"];
-
-// Date object:
-const date = new Date("2022-03-25");
- 
+console.log(`My Name is ${name} My Age is ${age} My Date of Birth ${dob}`);
 
 
-// JavaScript String Methods
-// String length
-let text = "Bangladesh";
-let length = text.length;
-
-// String slice()
-let part = text.slice(2, 5);
-console.log(part)
-
-// String substring()
-// String substr()
-// String replace()
-// String replaceAll()
-// String toUpperCase()
-// String toLowerCase()
-// String concat()
-// String trim()
-let text1 = "      Hello World!      ";
-let text2 = text1.trim();
-console.log(text2)
-// String trimStart()
-// String trimEnd()
-// String padStart()
-// String padEnd()
-// String charAt()
-// String charCodeAt()
-// String split()
-let text5 = "a,b,c,d,e,f";
-const myArray = text5.split(",");
-console.log(myArray[4]);
-
-
-
-// JavaScript Array Methods
-// Array length
-// Array toString()
-// Array pop()
-// Array push()
-// Array shift()
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
-console.log(fruits)
-fruits.shift();
-console.log(fruits)
-// Array unshift()
-// Array join()
-// Array delete()
-// Array concat()
-// Array flat()
-// Array splice()
-// Array slice()
+let Num1 = 10;
+let Num2 = 20;
+console.log(`${Num1} + ${Num2} = ${Num1 + Num2}`);
