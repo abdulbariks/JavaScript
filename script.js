11524f8aa981f7f9687930a1228eb7270e3d489a
Abdul Barik <abdulbarik1997m@gmail.com>
2023-10-10 03:08:39 +0600

    JavaScript JSON

--- script.js
+++ script.js
@@ -1,48 +1,24 @@
-// JavaScript Regular Expressions
+// JavaScript JSON
+
+let text = '{ "employees" : [' +
+'{ "firstName":"John" , "lastName":"Doe" },' +
+'{ "firstName":"Anna" , "lastName":"Smith" },' +
+'{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+
+let obj = JSON.parse(text);
+
+console.log(obj);
 
 
-
-let re;
-let str; 
-
-re = "Visitssa W3Schools!";
-str = "W3Schools"
-
-re = "Visi W3Schools!";
-str = /w3schools/i;
-
-re ="re, green, red, green, gren, gr, blue, yellow";
-
-console.log(re.search(str));
-console.log(re.replace(/w3schools/i, "Barik School"));
-console.log(re.match(/\d/g));
-console.log(re.match(/(red | green)/g));
-
-// Costal Code
-re = /^[0-9]{4}$/
-str = "5500";
-
-
-// Phone number
-//017386014909  +8801738601909 8801738601909
-re = /^(\+)?(88)?01[0-9]{9}$/;
-str = "01738601909"
-
-
-// Email
-re = /^([a-zA-Z0-9]\.?)+[^\.]@([a-zA-Z0-9]\.?)+[^\.]$/;
-str ="barik.34@gmail.com.bd"
-
-
-
-console.log(re.exec(str));
-console.log(re.test(str));
-
-reTest(re, str);
-function reTest(re, str){
-  if (re.test(str)) {
-    console.log(`'${str}' Matches '${re.source}'`);
-  } else {
-    console.log(`'${str}'  Doesn't Matches '${re.source}'`);
-  }
+let text1 = {
+  name: "abdul Barik",
+  age : 26,
+  city :"dhaka"
 }
+
+console.log(text1);
+
+let text_json = JSON.stringify(text1);
+console.log(text_json);
+let text_obj = JSON.parse(text_json);
+console.log(text_obj);
