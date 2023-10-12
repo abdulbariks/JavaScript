57462283801dbad867f0b8602c7d33cbb7a79234
Abdul Barik <abdulbarik1997m@gmail.com>
2023-10-13 03:37:27 +0600

    The var, let and const keywords

--- script.js
+++ script.js
@@ -1,27 +1,66 @@
-// Fetch API
+// The var, let and const keywords 
+
+var x = 2;     // Allowed
+var x = 3;     // Allowed
+x = 4;         // Allowed
 
 
-let file = "ajax_info.txt";
+let y = 2;     // Allowed
+// let y = 3;     // Not Allowed
+y = 4;         // Allowed
 
 
-fetch(file)
-.then(x => x.text())
-.then(y => console.log(y))
+const z = 10;  // Allowed
+// const z = 5;    // Not Allowed
+//  z = 10;         // Not Allowed
 
 
 
-// async function getText(file) {
-//   let x = await fetch(file);
-//   let y = await x.text();
-//   console.log(y);
+
+console.log(`Golobal Scope: `, x, y, z );
+
+function test() {
+  var x = 10;
+  let y = 20;
+  const z = 30;
+
+  console.log(`Functiuon Scope:`, x,y,z);
+}
+
+test()
+
+
+console.log(`Golobal Scope: `, x, y, z );
+
+
+if (true) {
+  var x = 40;
+  let y = 50;
+  const z = 60;
+
+  console.log(`if Scope: `, x, y, z );
+}
+
+console.log(`Golobal Scope: `, x, y, z );
+
+
+for (var x = 0; x < 8; x++) {
+  console.log(x);
+  
+}
+
+console.log(`Golobal Scope: `, x, y, z );
+
+for (let x = 0; x < 10; x++) {
+  console.log(x);
+  
+}
+
+console.log(`Golobal Scope: `, x, y, z );
+
+// for (const x = 0; x < 10; x++) {
+//   console.log(x);
+  
 // }
 
-getText("ajax_info.txt")
-
-
-
-async function getText(file) {
-  let myObject = await fetch(file);
-  let myText = await myObject.text();
-   console.log(myText);
-}
+// console.log(`Golobal Scope: `, x, y, z );
