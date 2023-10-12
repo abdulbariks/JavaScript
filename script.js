6cf07a01c064ff1ff26c2b1e6b5ca5ef924ec909
Abdul Barik <abdulbarik1997m@gmail.com>
2023-10-13 01:16:20 +0600

    JavaScript Callbacks, Asynchronous JavaScript, JavaScript Promises

--- script.js
+++ script.js
@@ -1,63 +1,91 @@
-// AJAX
-
-
-let xhttp = new XMLHttpRequest();
-console.log(xhttp );
-
-xhttp.onload = function() {
-  console.log(this.responseText);
+// JavaScript Callbacks
+// Function Sequence
+function myDisplayer(some) {
+  console.log(some);
 }
 
-xhttp.open("GET", "ajax_info.txt");
-xhttp.send();
+function myFirst() {
+  myDisplayer("Hello");
+}
+
+function mySecond() {
+  myDisplayer("Goodbye");
+}
+
+setTimeout(function myFirst() {
+  myDisplayer("Hello");
+},9000);
+
+setTimeout(function mySecond() {
+  myDisplayer("Goodbye");
+}, 7000);
 
 
-function loadDoc() {
-  const xhttp = new XMLHttpRequest();
-  xhttp.onload = function() {
-    document.getElementById("demo").innerHTML =
-    this.responseText;
+myFirst();
+mySecond();
 
-    console.log(this.responseText);
+
+// Callback Functions
+function myResult(something) {
+  console.log(something);
+}
+
+
+function mySum(num1, num2, myCallback) {
+  let sum = num1+num2;
+  myCallback(sum)
+}
+
+mySum(5,4,myResult);
+
+
+
+// setInterval(myFunction, 1000);
+// function myFunction() {
+//   let d = new Date();
+//   document.getElementById("demo").innerHTML=
+//   d.getHours() + ":" +
+//   d.getMinutes() + ":" +
+//   d.getSeconds();
+// }
+
+// JavaScript Functions
+setTimeout(function() { 
+  myFunction("I love JavaScript !!!"); 
+}, 3000);
+
+function myFunction(value) {
+  console.log(value);
+}
+
+// JavaScript Promise
+
+function mypromiseresult(value){
+  console.log(value);
+}
+const myPromise = new Promise(function(myResolve, myReject) {
+  let x = 5;
+  if (x == 0) {
+    myResolve("OK");
+  } else {
+    myReject("Error");
   }
-  xhttp.open("GET", "ajax_info.txt",true);
-  xhttp.send();
+});
+
+
+// myPromise.then(
+//   function(value) {
+//   console.log(value);
+// },
+// function(err) {
+//   console.log(err);
+// }
+
+myPromise.then(
+  function(value) {
+  mypromiseresult(value)
+},
+function(err) {
+  mypromiseresult(err)
 }
-
-
-
-
-
-
-
-document.getElementById("get_data").addEventListener("click", loadJokes)
-
-function loadJokes() {
-  const xhttp = new XMLHttpRequest();
-
-  xhttp.onprogress = function() {
-    document.getElementById('output').innerHTML = "<h3> Loading.......</h3>"
-  }
-
-  xhttp.onload= function() {
-    if (this.status == 200) {
-      let data = JSON.parse(this.responseText);
-      let jokes= data.value
-      console.log(data);
-      // console.log(jokes);
-
-       let output = "<ol>";
-      data.forEach(function(item) {
-        console.log(item);
-        output += `<li>${item}</li>`
-      });
-       output += "</ol>";
-
-       document.getElementById('output').innerHTML = output;
-    }
-  };
-  // xhttp.open("GET", "https://api.icndb.com/jokes",true);
-  // xhttp.open("GET", "https://api.chucknorris.io/jokes/random/",true);
-  xhttp.open("GET", "https://api.chucknorris.io/jokes/categories",true);
-  xhttp.send();
-}
+);
