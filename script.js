e15989de5e50b576813e4bc6b56f5aeb2aae7118
Abdul Barik <abdulbarik1997m@gmail.com>
2023-10-14 03:08:27 +0600

    Arrow Function & Template Literals

--- script.js
+++ script.js
@@ -1,66 +1,35 @@
-// The var, let and const keywords 
+// Arrow Function
 
-var x = 2;     // Allowed
-var x = 3;     // Allowed
-x = 4;         // Allowed
-
-
-let y = 2;     // Allowed
-// let y = 3;     // Not Allowed
-y = 4;         // Allowed
-
-
-const z = 10;  // Allowed
-// const z = 5;    // Not Allowed
-//  z = 10;         // Not Allowed
-
-
-
-
-console.log(`Golobal Scope: `, x, y, z );
-
-function test() {
-  var x = 10;
-  let y = 20;
-  const z = 30;
-
-  console.log(`Functiuon Scope:`, x,y,z);
+hello = function() {
+  return "Abdul Barik";
 }
 
-test()
+console.log(hello());
 
 
-console.log(`Golobal Scope: `, x, y, z );
-
-
-if (true) {
-  var x = 40;
-  let y = 50;
-  const z = 60;
-
-  console.log(`if Scope: `, x, y, z );
+hello = ()=>{
+  return "Abdul Barik"
 }
 
-console.log(`Golobal Scope: `, x, y, z );
+console.log(hello());
 
 
-for (var x = 0; x < 8; x++) {
-  console.log(x);
-  
+
+hello = () => "Abdul Barik"
+console.log(hello());
+
+
+hello = (val) => "Hello " + val;
+console.log(hello("Barik"));
+
+
+
+let person = () =>{
+  let name = "Abdul Barik";
+  let age = 26;
+
+  // Template Literals use back-ticks (``) 
+  console.log(`Name is ${name} & Age ${age}`);
 }
 
-console.log(`Golobal Scope: `, x, y, z );
-
-for (let x = 0; x < 10; x++) {
-  console.log(x);
-  
-}
-
-console.log(`Golobal Scope: `, x, y, z );
-
-// for (const x = 0; x < 10; x++) {
-//   console.log(x);
-  
-// }
-
-// console.log(`Golobal Scope: `, x, y, z );
+person()
